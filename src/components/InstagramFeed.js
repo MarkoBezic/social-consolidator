25c2f71e14f9b4c1b5d3049a241b8fd0c035ff62
markobezic <marko.bezic@gmail.com>
2021-03-12 22:35:19 -0500

    added form asking for twitter username, stores in firbase and displays feed on submit

---
+++ src/components/InstagramFeed.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InstagramFeed = () => {
+  return (
+    <div className="p-3 ">
+      <p className="p-2 border border-danger w-25">
+        Instagram Feed Placeholder
+      </p>
+    </div>
+  );
+};
+
+export default InstagramFeed;
--- src/components/TwitterFeed.js
+++ src/components/TwitterFeed.js
@@ -1,20 +1,89 @@
-import React from "react";
-import { Timeline } from "react-twitter-widgets";
+import React, { Component } from 'react'
+import { userDataRef } from '../firebase'
+import { Timeline } from 'react-twitter-widgets'
 
-const TwitterFeed = () => {
-  return (
-    <div>
-      <Timeline
-        dataSource={{
-          sourceType: "profile",
-          screenName: "Marko_Bezic",
-        }}
-        options={{
-          height: "500",
-        }}
-      />
-    </div>
-  );
-};
+class TwitterFeed extends Component {
+  state = {
+    twitterUser: '',
+    tempTwitterUser: '',
+  }
 
-export default TwitterFeed;
+  handleUserInput = e => {
+    e.preventDefault()
+    this.setState({
+      tempTwitterUser: e.target.value,
+    })
+  }
+
+  createUserRecordInFirebase = () => {
+    const userObject = {
+      twitterId: this.state.tempTwitterUser,
+    }
+    userDataRef.doc().set(userObject)
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.createUserRecordInFirebase()
+    this.setState({
+      twitterUser: this.state.tempTwitterUser,
+    })
+  }
+
+  handleChangeUser = () => {
+    this.setState({
+      twitterUser: '',
+    })
+  }
+
+  render() {
+    return (
+      <div className="p-3 border w-50">
+        {this.state.twitterUser ? (
+          <div>
+            <Timeline
+              dataSource={{
+                sourceType: 'profile',
+                screenName: this.state.twitterUser,
+              }}
+              options={{
+                height: '500',
+              }}
+            />
+            <button
+              className="btn btn-primary mx-1"
+              type="submit"
+              onClick={this.handleChangeUser}
+            >
+              Change account
+            </button>
+          </div>
+        ) : (
+          <div className="d-flex flex-column ">
+            <label for="twitter-name">Twitter account:</label>
+            <form onSubmit={e => this.handleSubmit(e)}>
+              <div className="row">
+                <div className="col-8">
+                  <input
+                    className="p-1"
+                    type="text"
+                    id="twitter-name"
+                    placeholder="Enter account name"
+                    onChange={this.handleUserInput}
+                  ></input>
+                </div>
+                <div className="col-4">
+                  <button className="btn btn-primary" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default TwitterFeed
