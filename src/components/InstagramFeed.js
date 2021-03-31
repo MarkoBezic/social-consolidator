11ed961ee6eebb827c2d7ff03655ba092abe617b
markobezic <marko.bezic@gmail.com>
2021-03-31 10:10:26 -0400

    add react-instagram-embed to instagram component

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React from 'react'
+import InstagramEmbed from 'react-instagram-embed'
 
 const InstagramFeed = () => {
   return (
     <div className="p-3 ">
-      <p className="p-2 border border-danger w-25">
-        Instagram Feed Placeholder
-      </p>
+      <InstagramEmbed
+        url="https://www.instagram.com/p/CMrsisdATlW/"
+        clientAccessToken="491132918562613|99bda1201d33320ec6ce9442ad18d1e0"
+        maxWidth={400}
+        hideCaption={false}
+        containerTagName="div"
+        protocol=""
+        injectScript
+        onLoading={() => {}}
+        onSuccess={() => {}}
+        onAfterRender={() => {}}
+        onFailure={() => {}}
+      />
     </div>
-  );
-};
+  )
+}
 
-export default InstagramFeed;
+export default InstagramFeed
